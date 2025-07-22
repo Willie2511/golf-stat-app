d3395bfcd64edc67d90bc01600dddf29a896bd6e
Willie Byrne <williambyrne@mac.home>
2025-07-22 21:57:29 +0100

    WIP end of day

---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],   // ← make sure this line is here!
+    theme: { extend: {} },
+    plugins: [],
+  };
